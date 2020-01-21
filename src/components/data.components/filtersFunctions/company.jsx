8a7a3c79082dd9ff20b0a9eaa94a7b6d851037b6
HOME PC <borislav.stefanov.1995@gmail.com>
2020-01-21 21:35:46 +0200

    bookmark home

--- src/components/data.components/filtersFunctions/company.jsx
+++ src/components/data.components/filtersFunctions/company.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import numeral from "numeral";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SmallCompanies = props => {
-  console.log(props);
+  console.log(props.singleCompany.IsBookmarked);
   let convertedIncome = 0;
   let planID;
   let isLarge;
@@ -19,8 +20,24 @@ const SmallCompanies = props => {
   }
 
   const addBookmark = e => {
+    const data = {
+      userGuid: sessionStorage.getItem("Guid"),
+
+      smallPlanID: props.singleCompany.SmallCompanyPlanID,
+
+      largePlanID: props.singleCompany.LargeCompanyPlanID
+    };
     e.target.classList.add("bookmarked");
-    console.log(e.target);
+    axios
+      .post(`http://pensionswebapi.azurewebsites.net/api/Bookmarks/Add`, data, {
+        headers: {
+          Authorization: "Basic " + sessionStorage.getItem("Token")
+        }
+      })
+      .then(res => {})
+      .catch(e => {
+        console.log(e);
+      });
   };
   return (
     <tr>
@@ -46,8 +63,8 @@ const SmallCompanies = props => {
         </Link>
       </td>
       <td>
-        {props.singleCompany.isBookarked ? (
-          "Yes"
+        {props.singleCompany.IsBookmarked ? (
+          <span className="fa fa-star bookmarked"></span>
         ) : (
           <span className="fa fa-star" onClick={addBookmark}></span>
         )}
